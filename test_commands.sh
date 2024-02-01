4204491f61d9be5ca283c1694e1ca4c663c543fc
Wujidadi <wujidadi@gmail.com>
2024-02-01 15:49:39 +0800

    Upgrade to 3.14 (PHP 8.1)

--- test_commands.sh
+++ test_commands.sh
@@ -6,10 +6,10 @@ docker buildx rm multiarch
 docker buildx create --name multiarch --use
 
 # Build the Docker image
-docker buildx build --no-cache --progress=plain --push --platform linux/amd64,linux/arm64/v8 --rm -t wujidadi/ap:3.13-php8.1 . 2>&1 | tee $D/docker-build-ap-3.13-php8.1.log
+docker buildx build --no-cache --progress=plain --push --platform linux/amd64,linux/arm64/v8 --rm -t wujidadi/ap:3.14-php8.1 . 2>&1 | tee $D/docker-build-ap-3.14-php8.1.log
 
 # Create test container and delete it finally
-docker run -d -p 50000:80 -it --name Test wujidadi/ap:3.13-php8.1
+docker run -d -p 50000:80 -it --name Test wujidadi/ap:3.14-php8.1
 # docker exec -it Test zsh
 docker exec -it Test vim --version | grep 'Included patches'
 docker exec -it Test nano -V | grep 'GNU nano, version'
