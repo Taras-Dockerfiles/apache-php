1070e1cd76f7431f238e0ee778893c6e680346f4
Wujidadi <wujidadi@gmail.com>
2024-04-02 13:54:14 +0800

    Re-add latest tag

--- test_commands.sh
+++ test_commands.sh
@@ -6,7 +6,7 @@ docker buildx rm multiarch
 docker buildx create --name multiarch --use
 
 # Build the Docker image
-docker buildx build --no-cache --progress=plain --push --platform linux/amd64,linux/arm64/v8 --rm -t wujidadi/ap:3.15-php8.3 . 2>&1 | tee $D/docker-build-ap-3.15-php8.3.log
+docker buildx build --no-cache --progress=plain --push --platform linux/amd64,linux/arm64/v8 --rm -t wujidadi/ap:3.15-php8.3 -t wujidadi/ap:latest . 2>&1 | tee $D/docker-build-ap-3.15-php8.3.log
 
 # Create testing container
 docker run -d -p 50000:80 -it --name Test wujidadi/ap:3.15-php8.3
