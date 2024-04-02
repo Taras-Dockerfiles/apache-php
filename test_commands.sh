1d6d4829cea07d0b77d53dd466ee16d7442b12f0
Wujidadi <wujidadi@gmail.com>
2024-04-02 10:15:13 +0800

    Upgrade to 3.15 (PHP 8.3)

--- test_commands.sh
+++ test_commands.sh
@@ -6,11 +6,12 @@ docker buildx rm multiarch
 docker buildx create --name multiarch --use
 
 # Build the Docker image
-docker buildx build --no-cache --progress=plain --push --platform linux/amd64,linux/arm64/v8 --rm -t wujidadi/ap:3.14-php8.3 . 2>&1 | tee $D/docker-build-ap-3.14-php8.3.log
+docker buildx build --no-cache --progress=plain --push --platform linux/amd64,linux/arm64/v8 --rm -t wujidadi/ap:3.15-php8.3 . 2>&1 | tee $D/docker-build-ap-3.15-php8.3.log
 
-# Create test container and delete it finally
-docker run -d -p 50000:80 -it --name Test wujidadi/ap:3.14-php8.3
-# docker exec -it Test zsh
+# Create testing container
+docker run -d -p 50000:80 -it --name Test wujidadi/ap:3.15-php8.3
+
+# Test the container outside itself by each command
 docker exec -it Test vim --version | grep 'Included patches'
 docker exec -it Test nano -V | grep 'GNU nano, version'
 docker exec -it Test apache2 -v | grep 'Server version'
@@ -20,5 +21,17 @@ docker exec -it Test composer -V
 docker exec -it Test node -v
 docker exec -it Test npm -v
 docker exec -it Test cat /opt/microsoft/msodbcsql18/etc/odbcinst.ini | grep 'Driver='
-docker stop Test
-docker rm Test
+
+# Test the container outside itself by one command
+docker exec -it Test bash -c "vim --version | grep 'Included patches'; \
+nano -V | grep 'GNU nano, version'; \
+apache2 -v | grep 'Server version'; \
+php -v | grep built; \
+php -i | grep swoole -1 | grep Version; \
+composer -V; \
+node -v; \
+npm -v; \
+cat /opt/microsoft/msodbcsql18/etc/odbcinst.ini | grep 'Driver='"
+
+# Delete the testing containter finally
+docker stop Test; docker rm Test
